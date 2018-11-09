d29cd8f4bad73964eaa2fe8b441948a73880c99d
Luke Secomb <luke.secomb@live.com>
2018-11-09 19:37:43 +1100

    fixed yarn / added gallery items / removed gallery-item component

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,23 +1,42 @@
 import React, { Component } from 'react'
+import { Link } from 'gatsby'
 import styled from 'styled-components'
-import GalleryItem from './gallery-item'
 import { colors, weight } from '../_variables.js'
 
-class Gallery extends Component {
-  
-  mapChildren() {
-    
-    return (
-      <GalleryItem>
-        <h1>Hola</h1>
-      </GalleryItem>
-    )
-  }
+const galleryItems = [
+  {
+    "title": "any.ideas",
+    "link": "any-ideas",
+    "color": "#626262",
+  },
+  {
+    "title": "WP Webhook Netlify Deploy",
+    "link": "wp-webhook-netlify-deploy",
+    "color": "#01BDAE",
+  },
+]
 
+class Gallery extends Component {
   render() {
+    console.log('galleryItems => ', galleryItems)
+    let keys = Object.keys(galleryItems);
+    console.log('keys => ', keys)
+
+    let returnMe = keys.map(key => {
+      return (
+        <div key={key}>
+          <Link to={galleryItems[key].link}>
+            <h2>{galleryItems[key].title}
+              <span className="underline" style={{backgroundColor: galleryItems[key].color}}/>
+            </h2>
+          </Link>
+        </div>
+      )
+    })
+
     return (
       <GalleryContainer>
-        {this.mapChildren()}
+        {returnMe}
       </GalleryContainer>
     )
   }
@@ -31,17 +50,62 @@ const GalleryContainer = styled.div`
   min-height: 100vh;
   margin: 0;
   padding: 0;
+  padding: 64px 148px;
   /* overflow: hidden; */
   position: relative;
 
   font-family: 'Raleway', sans-serif;
-
-  /* background: rgba(20,20,20,1);
-  background: -moz-linear-gradient(-45deg, rgba(20,20,20,1) 0%, rgba(44,44,44,1) 100%);
-  background: -webkit-gradient(left top, right bottom, color-stop(0%, rgba(20,20,20,1)), color-stop(100%, rgba(44,44,44,1)));
-  background: -webkit-linear-gradient(-45deg, rgba(20,20,20,1) 0%, rgba(44,44,44,1) 100%);
-  background: -o-linear-gradient(-45deg, rgba(20,20,20,1) 0%, rgba(44,44,44,1) 100%);
-  background: -ms-linear-gradient(-45deg, rgba(20,20,20,1) 0%, rgba(44,44,44,1) 100%);
-  background: linear-gradient(135deg, rgba(20,20,20,1) 0%, rgba(44,44,44,1) 100%);
-  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#141414', endColorstr='#2c2c2c', GradientType=1 ); */
+  display: grid;
+  grid-gap: 12px;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr;
+  > div {
+    border: 2px solid ${colors.gold};
+    transition: .5s;
+    
+    background-color: ${colors.black_one};
+    &:hover {
+      border: 2px solid initial;
+      transition: .5s;
+      a {
+        border-width: 2px;
+        transition: .5s;
+        h2 {
+          color: ${colors.gold};
+          transition: .5s;
+          .underline {
+            opacity: 1;
+            transition: .5s;
+          }
+        }
+      }
+    }
+    a {
+      width: 100%;
+      height: 100%;
+      display: grid;
+      text-align: center;
+      align-items: center;
+      justify-content: center;
+      text-decoration: none;
+      transition: .5s;
+      h2 {
+        position: relative;
+        margin: 0;
+        transition: .5s;
+        color: ${colors.gold};
+        .underline {
+          opacity: 0;
+          position: absolute;
+          bottom: -24px;
+          left: 0;
+          right: 0;
+          height: 5px;
+          width: 48px;
+          margin: 0 auto;
+          transition: .5s;
+        }
+      }
+    }
+  }
 `
