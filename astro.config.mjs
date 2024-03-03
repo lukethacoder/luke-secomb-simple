4a5c9c252e43f3a2147809cdb94b218efa20ad7f
Luke Secomb <luke.secomb@live.com>
2024-03-03 15:18:20 +1100

    📀 add placeholder image blurring / add skeleton loading to lastfm widget

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import partytown from '@astrojs/partytown'
+import { astroImageTools } from 'astro-imagetools'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
+    astroImageTools,
     partytown({
       // Adds dataLayer.push as a forwarding-event.
       config: {
