3a7df5c3285d7a5928863fd4b45239146913ec0d
Luke Secomb <luke.secomb@live.com>
2025-03-27 01:42:05 +1100

    🚧 wip - bump packages / foundational rebuild work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,27 @@
 import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import partytown from '@astrojs/partytown'
+import tailwindcss from '@tailwindcss/vite'
+// import partytown from '@astrojs/partytown'
 import { astroImageTools } from 'astro-imagetools'
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  markdown: {
+    shikiConfig: {
+      // TODO: add custom theme
+      theme: 'one-dark-pro',
+      wrap: true,
+    },
+  },
   integrations: [
-    tailwind(),
     astroImageTools,
-    partytown({
-      // Adds dataLayer.push as a forwarding-event.
-      config: {
-        forward: ['dataLayer.push', 'goatcounter.count'],
-      },
-    }),
+    // partytown({
+    //   // Adds dataLayer.push as a forwarding-event.
+    //   config: {
+    //     forward: ['dataLayer.push', 'goatcounter.count'],
+    //   },
+    // }),
   ],
 })
